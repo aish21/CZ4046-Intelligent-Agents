bf0e324e1e1faa66604b96ff861a2fc8ed35c91e
aish21 <aish.akshu@gmail.com>
2023-03-11 22:34:58 +0800

    Add all constants

--- controller/GridConstants.java
+++ controller/GridConstants.java
@@ -10,5 +10,13 @@ public final class GridConstants {
     public static final String CELL_SEPARATOR = ";";
     public static final String ROW_COLUMN_SEPARATOR = ",";
 
+    // Define the start position of the Agent
+    public static final int AGENT_INIT_COL = 2;
+	public static final int AGENT_INIT_ROW = 3;
+
+    // Define the environment data - placement of special cells
+    public static final String GREEN_CELLS = "0,0; 2,0; 5,0; 3,1; 4,2; 5,3";
+	public static final String BROWN_CELLS = "1,1; 5,1; 2,2; 3,3; 4,4";
+	public static final String WALL_CELLS = "1,0; 4,1; 1,4; 2,4; 3,4";
     
 }
---
+++ controller/IterationConstants.java
@@ -0,0 +1,32 @@
+package controller;
+
+public final class IterationConstants {
+    
+    // Define rewards for different cells
+    public static final double REWARD_WHITE = -0.040;
+    public static final double REWARD_GREEN = 1.000;
+    public static final double REWARD_BROWN = -1.000;
+    public static final double WALL_COLLISION = 0.000;
+
+    // Define the transition probabilities for the agent
+    public static final double INTENDED_PROBABILITY = 0.800;
+    public static final double RIGHT_PROBABILITY = 0.100;
+    public static final double LEFT_PROBABILITY = 0.100;
+
+    // Define the discount factor
+    public static final double DISCOUNT_FACTOR = 0.990;
+
+    // Define the maximum value of reward    
+    public static final double MAXIMUM_REWARD = 1.000;
+
+    // Define the parameters for maximum error permitted  
+    public static final double MAXIMUM_ALLOWED_ERROR = 30;
+    public static final double MAXIMUM_ALLOWABLE_DISCOUNTED_ERROR = MAXIMUM_ALLOWED_ERROR * MAXIMUM_REWARD;
+
+    // Define the number of times the Bellman algorithm is executed
+    public static final int K = 10;
+
+    // Define the upper bound on utility values
+    public static final double UTILITY_UPPER_BOUND = MAXIMUM_REWARD / (1 - DISCOUNT_FACTOR);
+    
+}
