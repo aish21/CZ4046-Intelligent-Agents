eefe76405fc09ca0e2b9d822c3fa43e7bd4b91e7
aish21 <aish.akshu@gmail.com>
2023-03-11 19:13:43 +0800

    Add updated files

---
+++ controller/GridConstants.java
@@ -0,0 +1,14 @@
+package controller;
+
+public final class GridConstants {
+
+    // Define the dimensions of the Grid
+    public static final int TOTAL_NUM_COLS = 6;
+    public static final int TOTAL_NUM_ROWS = 6;
+
+    // Define separators for grid cells and row-columns
+    public static final String CELL_SEPARATOR = ";";
+    public static final String ROW_COLUMN_SEPARATOR = ",";
+
+    
+}
