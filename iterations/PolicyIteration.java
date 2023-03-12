e999fa13ef87cd341c7eff53c873049e8f2f91a8
aish21 <aish.akshu@gmail.com>
2023-03-13 00:27:56 +0800

    Add policy iter

---
+++ iterations/PolicyIteration.java
@@ -0,0 +1,129 @@
+package iterations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import controller.ShowOutput;
+import controller.SaveOutput;
+import controller.UtilityController;
+import grid.Utility;
+import grid.AgentState;
+import grid.Grid;
+import grid.AgentAction;
+
+public class PolicyIteration {
+    
+    public static Grid gridEnvironment;
+	private static List<Utility[][]> utilityList;
+	private static AgentState[][] grid;
+	private static int iterations = 0;
+	private static double convergeThreshold;
+	private static boolean isValueIteration = true;
+
+	public static void main(String[] args) {
+
+		// Initialize grid environment
+		gridEnvironment = new Grid();
+		grid = gridEnvironment.getGrid();
+
+		// Execute value iteration
+		runPolicyIteration(grid);
+
+		// Display experiment results
+		displayResults();
+
+		// Save utility estimates to csv file for plotting
+		SaveOutput.writeToFile(utilityList, "policy_iteration_utilities");
+	}
+
+    public static void runPolicyIteration(final AgentState[][] grid) {
+
+		Utility[][] currUtilArr = new Utility[Const.NUM_COLS][Const.NUM_ROWS];
+		Utility[][] newUtilArr = new Utility[Const.NUM_COLS][Const.NUM_ROWS];
+
+		// Initialize default utilities and policies for each state
+		for (int col = 0; col < Const.NUM_COLS; col++) {
+			for (int row = 0; row < Const.NUM_ROWS; row++) {
+				newUtilArr[col][row] = new Utility();
+				if (!grid[col][row].isWall()) {
+					AgentAction randomAction = AgentAction.getRandomAction();
+					newUtilArr[col][row].setAction(randomAction);
+				}
+			}
+		}
+
+		// List to store utilities of every state at each iteration
+		utilityList = new ArrayList<>();
+
+		// Used to check if the current policy value is already optimal
+		boolean unchanged = true;
+
+		do {
+
+			UtilityController.updateUtilites(newUtilArr, currUtilArr);
+
+			// Append to list of Utility a copy of the existing actions & utilities
+			Utility[][] currUtilArrCopy =
+			new Utility[Const.NUM_COLS][Const.NUM_ROWS];
+			UtilityController.updateUtilites(currUtilArr, currUtilArrCopy);
+			utilityList.add(currUtilArrCopy);
+			
+
+			// Policy estimation based on the current actions and utilities
+			newUtilArr = UtilityController.estimateNextUtilities(currUtilArr, grid);
+
+			unchanged = true;
+
+			// For each state - Policy improvement
+			for (int row = 0; row < Const.NUM_ROWS; row++) {
+				for (int col = 0; col < Const.NUM_COLS; col++) {
+
+					// Calculate the utility for each state, not necessary to calculate for walls
+					if (!grid[col][row].isWall()) {
+						// Best calculated action based on maximizing utility
+						Utility bestActionUtil =
+						UtilityController.getBestUtility(col, row, newUtilArr, grid);
+
+						// Action and the corresponding utility based on current policy
+						AgentAction policyAction = newUtilArr[col][row].getAction();
+						Utility policyActionUtil = UtilityController.getFixedUtility(policyAction, col, row, newUtilArr, grid);
+
+						if((bestActionUtil.getUtil() > policyActionUtil.getUtil())) {
+							newUtilArr[col][row].setAction(bestActionUtil.getAction());
+							unchanged = false;
+						}
+					}
+				}
+			}
+			iterations++;
+
+		} while (!unchanged);
+	}
+
+
+	private static void displayResults() {
+		// Final item in the list is the optimal policy derived by policy iteration
+		int latestUtilities = utilityList.size() - 1;
+		final Utility[][] optimalPolicy =
+		utilityList.get(latestUtilities);
+
+		// Displays the Grid Environment
+		ShowOutput.dispGrid(grid);
+
+		// Displays the experiment setup
+		ShowOutput.displayExperimentSetup(isValueIteration, 0);
+
+		// Display total number of iterations required for convergence
+		ShowOutput.displayIterationsCount(iterations);
+
+		// Display the utilities of all the (non-wall) states
+		ShowOutput.displayUtilities(grid, optimalPolicy);
+
+		// Display the optimal policy
+		ShowOutput.displayPolicy(optimalPolicy);
+
+		// Display the utilities of all states
+		ShowOutput.displayUtilitiesGrid(optimalPolicy);
+	}
+
+}
